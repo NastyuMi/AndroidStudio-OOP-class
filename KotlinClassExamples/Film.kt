8526738ad7ddb27a5637d9ef742b229f3c110b88
NastyuMi <90446202+NastyuMi@users.noreply.github.com>
2022-10-27 22:54:37 +0300

    second updates

---
+++ KotlinClassExamples/Film.kt
@@ -0,0 +1,19 @@
+package com.KotlinClassExamples
+
+abstract class Film(type: TypeOfFilm) : CinematicWorkOfArt(type) {
+
+    override fun nameOfFilm()
+    {
+        super.nameOfFilm()
+        print("film : ")
+    }
+
+    open fun aboutFilm()
+    {
+        nameOfFilm();
+        println(type)
+        println("Genre : ${genre()}")
+        println("Year of release : ${yearOfRelease()}")
+        shortDescription()
+    }
+}
